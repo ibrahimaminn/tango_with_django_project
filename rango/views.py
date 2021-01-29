bc7f666ea270e31bdfde2537cc1aaa33ef576553
Ibrahim Amin <ibrahimamin23@hotmail.com>
2021-01-29 04:08:52 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,5 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
